a8ad2271f12aea350b83b307487e5f83cea4f2bf
Adam Mose <AoD.Nik@gmail.com>
2019-04-17 16:05:00 +0200

    17.04.2019
    Icons:
    - Added icons for xbox one, ps4, mouse and keyboard inputs
    
    Player Controller:
    - Added funcion to recognize if the gamepad is in use
    
    Fire particle:
    - Modified fire particle from Starting Content to be more optimized
    
    GameMode:
    - Gamemode will now tick even when game is paused
    
    UIButton + UIArrowLeft + UIArrowRight:
    - Added new button template that works with gamepad, mouse and keyboard
    
    UICheckBox:
    - Added new checkbox template that works with gamepad, mouse and keyboard
    
    Graphics:
    - Added graphics for Default, Hovered, Pressed and Disabled for:
    • Normal button
    • Left arrow butotn
    • Right arrow button
    • CheckBox
    • Return / Go back button
    
    Bottom Info Bar widget:
    -  Created widget to give user infomation about possible keyboard or gamepad inputs
    
    MainMenu Widget + Pause Menu Widget + Settings Widget + Level Selector Widget:
    - All widgets now support gamepad controls
    
    Level Selector:
    - Moved camera
    - Added foliage
    
    Main Menu:
    - Added foliage
    
    Flower:
    - Added new mesh for flowers
    - Added new materials for flowers
    
    Grass:
    - Added new mesh for grass
    - Added new materials for grass

--- Source/TreasureAdventure/TAPlayerController.h
+++ Source/TreasureAdventure/TAPlayerController.h
@@ -7,11 +7,39 @@
 #include "TAPlayerController.generated.h"
 
 /**
- * 
+ *  
  */
 UCLASS()
 class TREASUREADVENTURE_API ATAPlayerController : public APlayerController
 {
 	GENERATED_BODY()
-	
+
+	ATAPlayerController();
+
+public:
+	UPROPERTY(Transient, BlueprintReadOnly)
+		bool bIsUsingGamepad;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+		bool bResetGamepadDetectionAfterNoInput;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (EditCondition = "bResetGamepadDetectionAfterNoInput"))
+		float GamepadTimeout;
+
+	virtual bool InputAxis(FKey Key, float Delta, float DeltaTime, int32 NumSamples, bool bGamepad) override;
+	virtual bool InputKey(FKey Key, EInputEvent EventType, float AmountDepressed, bool bGamepad) override;
+	virtual void PlayerTick(float DeltaTime) override;
+
+protected:
+	FORCEINLINE void _UpdateGamepad(bool bGamepad)
+	{
+		bIsUsingGamepad = bGamepad;
+		if (bGamepad)
+		{
+			LastGamepadInputTime = GetWorld()->TimeSeconds;
+		}
+	}
+
+	UPROPERTY(Transient, BlueprintReadOnly)
+		float LastGamepadInputTime;
 };
