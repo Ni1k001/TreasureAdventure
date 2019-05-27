6e308517a9437e7f46ab1f7b4eec4e30ab557637
Adam Mose <AoD.Nik@gmail.com>
2019-05-27 21:56:10 +0200

    27.05.2019
    Levels:
    - Fixed minor bugs
    
    Buttons:
    - Made buttons more visibility different when selected / hovered
    
    Settings:
    - Fixed missing display name for Effects Quality

--- Source/TreasureAdventure/TreasureAdventureGameMode.cpp
+++ Source/TreasureAdventure/TreasureAdventureGameMode.cpp
@@ -7,16 +7,7 @@
 
 ATreasureAdventureGameMode::ATreasureAdventureGameMode()
 {
-	// set default pawn class to our Blueprinted character
-	//static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
-	//if (PlayerPawnBPClass.Class != NULL)
-	//{
-	//	DefaultPawnClass = PlayerPawnBPClass.Class;
-	//}
-
-
-	
-
+	// set default pawn class
 	DefaultPawnClass = APlayerCharacter::StaticClass();
 }
 
